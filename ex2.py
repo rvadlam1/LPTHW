3f9a462e36a9ef89a1d872523f69db1822f84550
rvadlam1 <coolraviteja@gmail.com>
2013-07-17 16:13:25 -0700

    exercise 2 - importance of commenting

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#Commenting is important for understanding the code at a later time.
+#It also helps a new programmer to understand the flow of the program.
+
+print "I could have code like this." # and this comment is ignored
+
+#Commenting can be used to hide certain code for testing purposes.
+#print "this will not run"
+
+print "this will run/print."
