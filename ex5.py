8e13a073214b3096d5d4a7762ae47aea1c894122
rvadlam1 <coolraviteja@gmail.com>
2013-07-17 17:11:23 -0700

    exercise 5 - Formatters

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name = 'Foobar'
+my_age = 25
+my_height = 180 # centimeters
+my_weight = 180 # lbs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He is %s centimeters tall." % my_height
+print "He weighs %s pounds." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
